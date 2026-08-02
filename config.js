d7da0ced3ec18f5a71a34eeeb53c13f3c61351a8
toughahtough-ux <toughahtough@gmail.com>
2026-08-02 16:07:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const CONFIG = {
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🐡', '🦐', '🐟', '🐠', '🦑', '🦀', '🦞', '🐬', '🐳', '🦈', '🏖️']    // Cute bear emojis
+        fishes: ['🐡', '🦐', '🐟', '🐠', '🦑', '🦀', '🦞', '🐬', '🐳', '🦈', '🏖️']    // Cute bear emojis
     },
 
     // Questions and answers
